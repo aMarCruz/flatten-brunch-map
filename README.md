5e620b5066a364035d6c1027276c33322912982d
amarcruz <amarcruzbox-git@yahoo.com>
2016-11-06 15:22:36 -0600

    v2.8.1 avoid depending on filename

--- README.md
+++ README.md
@@ -5,6 +5,10 @@
 
 Creates the object to return in a [Brunch](http://brunch.io) plugin, with an optional sourcemap, merged with the preceding sourcemap if necessary.
 
+**IMPORTANT:**
+
+v2.8.1 have a breaking change, please read the [What's New](#whats-new) section.
+
 ## Install
 
 ```bash
@@ -29,22 +33,23 @@ class MyPlugin {
 
   constructor (config) {
     this.options = config.plugins.myPlugin || {}
-
     // Say to myCompiler if user wants sourcemap
     this.options.sourceMaps = !!config.sourceMaps
   }
 
   compile (file) {
-    try {
-      // Do the plugin logic and then call flattenMap with the
-      // received param and the generated code and sourcemap.
-      const output = myCompiler(file.data, this.options)
-      const result = flattenMap(file, output.code, output.map)
+    return new Promise((resolve, reject) => {
+      try {
+        // Do the plugin logic and then call flattenMap with the
+        // received param and the generated code and sourcemap.
+        const output = myCompiler(file.data, this.options)
+        const result = flattenMap(file, output.code, output.map)
 
-      return Promise.resolve(result)
-    } catch (error) {
-      return Promise.reject(error)
-    }
+        resolve(result)
+      } catch (error) {
+        reject(error)
+      }
+    })
   }
 
 }
@@ -56,6 +61,19 @@ MyPlugin.prototype.type = 'javascript'
 module.exports = MyPlugin
 ```
 
+
+## What's New
+
+From v2.8.1, the plugin does not depend on filename.
+
+Normalization of file names that were made in previous versions (conversion of `\` to `/`) no longer occurs and the names are transferred as-is.
+
+Also from v2.8.1, the format of the `map` property of the returned object is the same as that received in the 3rd parameter. That is, if an object is received, an object is returned instead of a JSON string.
+
+This helps compatibility between different tools, but the caller must ensure consistency of the filenames.
+
+
+---
 Like it? Don't forget your star.
 
 [npm-image]:      https://img.shields.io/npm/v/flatten-brunch-map.svg
--- index.js
+++ index.js
@@ -3,10 +3,6 @@
 const SourceMapGenerator = require('source-map').SourceMapGenerator
 const SourceMapConsumer  = require('source-map').SourceMapConsumer
 
-function normalize (path) {
-  return path && path.replace(/\\/g, '/')
-}
-
 function assertProperty (sourceMap, propertyName) {
   if (!sourceMap.hasOwnProperty(propertyName)) {
     const e = new Error(`Source map to be applied is missing the "${propertyName}" property`)
@@ -14,15 +10,50 @@ function assertProperty (sourceMap, propertyName) {
   }
 }
 
+function transfer (fromSourceMap, toSourceMap, asString) {
+  const smFrom    = new SourceMapConsumer(fromSourceMap)
+  const smTo      = new SourceMapConsumer(toSourceMap)
+  const smResult  = new SourceMapGenerator({
+    file: fromSourceMap.file
+  })
+
+  smFrom.eachMapping(function (mapping) {
+    const generatedPosition = {
+      line: mapping.generatedLine,
+      column: mapping.generatedColumn
+    }
+    const fromOriginalPosition = {
+      line: mapping.originalLine,
+      column: mapping.originalColumn
+    }
+    // from's generated position -> to's original position
+    const originalPosition = smTo.originalPositionFor(fromOriginalPosition)
+
+    if (originalPosition.source !== null) {
+      smResult.addMapping({
+        source: originalPosition.source,
+        name : originalPosition.name,
+        generated: generatedPosition,
+        original: originalPosition
+      })
+    }
+  })
+
+  return asString ? smResult.toString() : smResult.toJSON()
+}
+
 /**
+ * Return a re-mapped source map string
+ *
  * @param   {object}       [sourceFile]  - The param received by the plugin
  * @param   {string}        compiled     - Processed or compiled code
  * @param   {object|string} sourceMap    - Generated source map
  * @returns {object} The resulting file object with flatten source map, if any.
  */
-function flattenMap (sourceFile, compiled, sourceMap) {
-  let fileName = sourceFile.path
-  let prevMap  = sourceFile.map
+function flattenBrunchMap (sourceFile, compiled, sourceMap) {
+  let asString = false
+  let prevMap = sourceFile.map
+  let newMap = sourceMap
 
   // make sure the current map is an object
   if (prevMap && (typeof prevMap == 'string' || prevMap instanceof String)) {
@@ -30,37 +61,35 @@ function flattenMap (sourceFile, compiled, sourceMap) {
   }
 
   // make sure the new map is an object
-  if (sourceMap && typeof sourceMap == 'string' || sourceMap instanceof String) {
-    sourceMap = JSON.parse(sourceMap)
+  if (newMap && (typeof newMap == 'string' || newMap instanceof String)) {
+    newMap = JSON.parse(newMap)
+    asString = true
   }
 
   const result = { data: compiled }
 
-  if (sourceMap) {
+  if (newMap) {
     // check source map properties
-    assertProperty(sourceMap, 'mappings')
-    assertProperty(sourceMap, 'sources')
+    assertProperty(newMap, 'mappings')
+    assertProperty(newMap, 'sources')
 
-    // fix paths if Windows style paths
-    sourceMap.file = normalize(sourceMap.file || fileName)
-    sourceMap.sources = sourceMap.sources.map(normalize)
+    // previous map?
+    if (prevMap && prevMap.mappings) {
 
-    if (prevMap && prevMap.mappings !== '') {
-      const generator = SourceMapGenerator.fromSourceMap(new SourceMapConsumer(sourceMap))
+      if (!newMap.file && prevMap.file) {
+        newMap.file = prevMap.file
+      }
+      result.map = transfer(newMap, prevMap, asString)
 
-      generator.applySourceMap(new SourceMapConsumer(prevMap), normalize(fileName))
-      sourceMap = JSON.parse(generator.toString())
+    } else {
+      result.map = sourceMap
     }
 
-    result.map = sourceMap
-
   } else if (prevMap) {
-
-    // seems that it is not neccesary
     result.map = prevMap
   }
 
   return result
 }
 
-module.exports = flattenMap
+module.exports = flattenBrunchMap
